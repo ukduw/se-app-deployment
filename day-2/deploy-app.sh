51aea58bdafa0a1c3c762bcbccc38acd7c028af9
ukduw <edhkmayo@gmail.com>
2025-11-18 10:34:10 -0500

    added basic version of app deploy; runs in foreground - TODO: refactor for pm2 to run in background

--- day-2/deploy-app.sh
+++ day-2/deploy-app.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+
+sudo git clone https://github.com/ukduw/se-test-app.git
+
+sudo apt install nginx -y
+sudo bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -"
+sudo apt install nodejs -y
+
+cd nodejs20-se-test-app-2025/app
+
+sudo npm install
+npm start app.js    # runs in foreground
+

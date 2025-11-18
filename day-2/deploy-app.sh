9bf49872d73669e83ece8f8c5efd8018bac694a3
ukduw <edhkmayo@gmail.com>
2025-11-18 11:23:32 -0500

    added reverse proxy automation via sed, after line 48

--- day-2/deploy-app.sh
+++ day-2/deploy-app.sh
@@ -8,7 +8,12 @@ sudo apt install nginx -y
 sudo bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -"
 sudo apt install nodejs -y
 
-cd nodejs20-se-test-app-2025/app
+cd /
+cd etc/nginx/sites-available
+sudo sed -i '48a\        proxy_pass http://localhost:3000;' default
+
+cd /
+cd ~/nodejs20-se-test-app-2025/app
 
 sudo npm install
 sudo npm install pm2 -g

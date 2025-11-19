6ce7e6ce7134f034f29ba41e4454d602b2ce7d24
ukduw <edhkmayo@gmail.com>
2025-11-19 11:50:50 -0500

    fixed syntax error in sed command

--- day-3/deploy-db.sh
+++ day-3/deploy-db.sh
@@ -12,7 +12,7 @@ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gp
 sudo apt update -y
 sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.1.5 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
 
-sudo sed -i 's/bindIp:.*/bindIp: 0.0.0.0' /etc/mongod.conf
+sudo sed -i 's/bindIp:.*/bindIp: 0.0.0.0/' /etc/mongod.conf
 
 sudo systemctl start mongod
 

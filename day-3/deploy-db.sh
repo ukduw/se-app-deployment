55ba71ee674456a5fb1abb0fda37f8a63cd369fe
ukduw <edhkmayo@gmail.com>
2025-11-19 10:58:52 -0500

    added db deploy script, bindIp in conf replaced with 0.0.0.0 for testing

--- day-3/deploy-db.sh
+++ day-3/deploy-db.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install gnupg -y
+sudo apt install curl -y
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor`
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.1.5 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
+
+sudo sed -i 's/  bindIp:*/  bindIp: 0.0.0.0' /etc/mongod.conf
+
+sudo systemctl start mongod
+

f74008c19bbca5a187d2be71ab6f717e28671cab
ukduw <edhkmayo@gmail.com>
2025-11-19 11:34:45 -0500

    fixed typo in curl

--- day-3/deploy-db.sh
+++ day-3/deploy-db.sh
@@ -7,7 +7,7 @@ sudo apt install curl -y
 
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor`
+   --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.1.5 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6

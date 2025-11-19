362c3e89b9366984840716fedc20bd90bfbad435
ukduw <edhkmayo@gmail.com>
2025-11-19 11:40:53 -0500

    added missing update line after creating list

--- day-3/deploy-db.sh
+++ day-3/deploy-db.sh
@@ -9,6 +9,7 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
+sudo apt update -y
 sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.1.5 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
 
 sudo sed -i 's/  bindIp:*/  bindIp: 0.0.0.0' /etc/mongod.conf

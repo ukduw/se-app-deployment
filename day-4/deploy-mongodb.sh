f10105b91cac7f3869d2326bb5d0dd78c3c43352
ukduw <edhkmayo@gmail.com>
2025-11-20 05:52:30 -0500

    added deploy db script

--- day-4/deploy-mongodb.sh
+++ day-4/deploy-mongodb.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install -y gnupg curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo apt update -y
+sudo apt install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.1.5 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
+    # latest release package: sudo apt install mongodb-org -y
+
+sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
+    # check with grep 'bindIp:' /etc/mongod.conf
+
+sudo systemctl start mongod
+sudo systemctl enable mongod 
+

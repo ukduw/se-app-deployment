d16ba959427b305c05b484e403a61bec753ee8c9
ukduw <edhkmayo@gmail.com>
2025-11-21 10:26:52 -0500

    added docker deploy script, TODO: deploy apps, schedule/enable as persistent services per container...

---
+++ docker-proj/deploy-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install -y ca-certificates curl gnupg
+
+# Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Docker repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+# 
